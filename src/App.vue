2cca39102848c543bcbc4053f3d0ea8aa31a6480
vovasap <rednotgreen2020@gmail.com>
2020-10-02 21:39:20 +0300

    feature(app.vue) add app content and styles

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,68 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="field"></div>
+
+    <div class="data-container">
+      <form>
+        <input type="text" placeholder="name" />
+        <input type="text" placeholder="color" />
+        <input type="text" placeholder="figure" />
+        <div class="buttons">
+          <button>Add</button>
+          <button>Delete</button>
+        </div>
+      </form>
+
+      <div class="table">table</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  components: {},
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
+  display: flex;
+  justify-content: center;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 10px;
+}
+.field {
+  width: 500px;
+  height: 500px;
+
+  border: 1px solid #555;
+  background: #eee;
+}
+form {
+  margin: 0 15px 15px;
+  display: flex;
+  flex-direction: column; /* max-width: 300px; */
+}
+input {
+  margin-bottom: 10px;
+}
+.buttons {
+  display: flex;
+  justify-content: space-evenly;
+}
+button {
+  width: 120px;
+  margin: 5px;
+}
+
+.table {
+  margin: 0 15px 15px;
 }
 </style>
